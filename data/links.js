e892b47424c6a4638eb121dd6c72ffe4f698a404
xanf-code <darshanaswathh@gmail.com>
2023-09-18 13:58:29 +0530

    Links done

---
+++ data/links.js
@@ -0,0 +1,52 @@
+export const data = [
+  {
+    link: "https://t.me/NothingPhone1",
+    name: "Nothing Phone (1) Global | Telegram",
+    tag: "Nothing Phone 1",
+  },
+  {
+    link: "https://t.me/NothingPhone2",
+    name: "Nothing Phone (2) Global | Telegram",
+    tag: "Nothing Phone 2",
+  },
+  {
+    link: "https://t.me/NothingPhonePhotography",
+    name: "Nothing Phone (1) Photography | Telegram",
+    tag: "Nothing Phone 1",
+  },
+  {
+    link: "https://t.me/NothingPhone2Photography",
+    name: "Nothing Phone (2) Photography",
+    tag: "Nothing Phone 2",
+  },
+  {
+    link: "https://forum.xda-developers.com/f/nothing-phone-1.12585/",
+    name: "Nothing Phone (1) | XDA",
+    tag: "Nothing Phone 1",
+  },
+  {
+    link: "https://forum.xda-developers.com/f/nothing-phone-2.12739/",
+    name: "Nothing Phone (2) | XDA",
+    tag: "Nothing Phone 2",
+  },
+  {
+    link: "https://drive.google.com/drive/folders/1NuApUuV_JjpfsWf81NlSkplLK4w591aL?usp=drive_link",
+    name: "Nothing Phone (2) Utilities | Drive",
+    tag: "Nothing Phone 2",
+  },
+  {
+    link: "https://drive.google.com/drive/folders/1CbAUsW2lFVkAlC2vG13r_cH-sSIsvNUi?usp=drive_link",
+    name: "Nothing Phone (2) Stock Roms | Drive",
+    tag: "Nothing Phone 2",
+  },
+  {
+    link: "https://drive.google.com/drive/folders/1z9tzclrQQ4eHi1dyXXT6AfcPQ_BaY9jo?usp=drive_link",
+    name: "Nothing Phone (2) Root | Drive",
+    tag: "Nothing Phone 2",
+  },
+  {
+    link: "https://drive.google.com/drive/folders/1DJBkaS9pYBIsdcAi25IbIk2qAL8hkVoS?usp=drive_link",
+    name: "Nothing Phone (2) Misc | Drive",
+    tag: "Nothing Phone 2",
+  },
+];
